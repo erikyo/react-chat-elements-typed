082221b2861d7d47fdeb848ab05ce957e71daa7b
Erik Golinelli <erik@codekraft.it>
2024-07-27 12:00:52 +0200

    storybook config tuning

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,12 +1,13 @@
 import type { Preview } from "@storybook/react";
 
+import "../src/tailwind.css";
 import "../src/styles.css";
 
 const preview: Preview = {
 	parameters: {
 		controls: {
 			matchers: {
-				color: /(background|color)$/i,
+				color: /(background|color|rgb)$/i,
 				date: /Date$/i,
 			},
 		},
