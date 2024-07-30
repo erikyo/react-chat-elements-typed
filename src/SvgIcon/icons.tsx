7d7f5251835ffb9cf82479ad5fb8fd1a4cbe500f
Erik Golinelli <erik@codekraft.it>
2024-07-30 11:39:26 +0200

    svg icons

--- src/SvgIcon/icons.tsx
+++ src/SvgIcon/icons.tsx
@@ -1,15 +1,42 @@
-import React, { type FC, type ReactNode, type SVGProps } from "react";
+import React, {
+	type CSSProperties,
+	type FC,
+	type ReactNode,
+	type SVGProps,
+} from "react";
 
 export interface SVGPropsWithChildren extends SVGProps<SVGSVGElement> {
 	children: ReactNode;
+	size?: number;
+	style?: CSSProperties;
 }
 
 const SVG: FC<SVGPropsWithChildren> = ({
 	children,
+	size = 24,
+	style = {},
+	className = "",
 	...props
 }: SVGPropsWithChildren) => {
-	// biome-ignore lint/a11y/noSvgWithoutTitle: <the title attribute will be provided>
-	return <svg {...props}>{children}</svg>;
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			role={"img"}
+			viewBox={`0 0 ${size} ${size}`}
+			height={size}
+			width={size}
+			{...props}
+			style={{
+				display: "inline-flex",
+				verticalAlign: "middle",
+				fill: "currentColor",
+				...style,
+			}}
+			className={className}
+		>
+			{children}
+		</svg>
+	);
 };
 
 export default SVG;
