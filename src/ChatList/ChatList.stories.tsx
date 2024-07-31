07f3f034c79c9ba329ee6d66ee965bd93d442b0e
Erik Golinelli <erik@codekraft.it>
2024-08-01 00:26:33 +0200

    the ChatList component style and the related story

--- src/ChatList/ChatList.stories.tsx
+++ src/ChatList/ChatList.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
 import ChatList from "./ChatList";
+import { chatListArray } from "../stories/example/utils/MessageTypes";
 
 const meta = {
 	component: ChatList,
@@ -11,5 +12,7 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-	args: {},
+	args: {
+		dataSource: Array.from({ length: 10 }).map((_, i) => chatListArray()),
+	},
 };
