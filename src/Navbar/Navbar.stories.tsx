fa752e17b9056ef790280a86178ddd3ac43d92b0
Erik Golinelli <erik@codekraft.it>
2024-08-08 19:35:54 +0200

    updated navbar - chatitem - dropdown style and stories

--- src/Navbar/Navbar.stories.tsx
+++ src/Navbar/Navbar.stories.tsx
@@ -1,25 +1,43 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
 import Navbar from "./Navbar";
+import { IconEmoji } from "../SvgIcon/IconEmoji";
 
 const meta = {
 	component: Navbar,
-} satisfies Meta<typeof Navbar>;
+};
 
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
+export const Default = {
 	args: {
 		left: "left",
 		right: "right",
-		center: "center",
+		center: (
+			<>
+				<IconEmoji />
+				<IconEmoji />
+				<IconEmoji />
+				<IconEmoji />
+			</>
+		),
 		style: {},
 	},
 };
 
-export const Dark: Story = {
+export const Light = {
+	args: {
+		left: "left",
+		right: "right",
+		center: "center",
+		type: "light",
+		style: {},
+	},
+};
+
+export const Dark = {
 	args: {
 		left: "left",
 		right: "right",
