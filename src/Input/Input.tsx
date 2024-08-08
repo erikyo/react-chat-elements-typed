b6a03457894dce76ac079e7b5849036c48db48c1
Erik Golinelli <erik@codekraft.it>
2024-08-08 19:37:55 +0200

    input component update - clear button - now it's a form - correctly handles the submit event

--- src/Input/Input.tsx
+++ src/Input/Input.tsx
@@ -1,9 +1,11 @@
 import type React from "react";
 import type { FC, LegacyRef, SyntheticEvent } from "react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./Input.css";
 import classNames from "classnames";
 import type { IInputProps } from "../types";
+import Button from "../Button/Button";
+import { MdClear } from "react-icons/md";
 
 const Input: FC<IInputProps> = ({
 	type = "text",
@@ -12,26 +14,38 @@ const Input: FC<IInputProps> = ({
 	maxHeight = 200,
 	autoHeight = true,
 	autofocus = false,
+	clearButton = true,
 	placeholder = "Type here...",
+	value: propValue = "",
 	style = {},
 	...props
 }) => {
+	const [value, setValue] = useState<string>(
+		propValue || props.defaultValue || "",
+	);
+
 	useEffect(() => {
 		if (autofocus) props.reference?.current?.focus();
+	}, [autofocus, props.reference?.current?.focus]);
 
-		if (props.clear instanceof Function) {
-			props.clear(clear);
-		}
-	}, [autofocus, props.clear, props.reference?.current?.focus]);
+	/** Update state if propValue changes */
+	useEffect(() => {
+		setValue(propValue);
+	}, [propValue]);
 
-	const onChangeEvent = ({
-		ev,
-		target,
-	}: {
-		ev?: SyntheticEvent;
-		target?: HTMLInputElement | HTMLTextAreaElement;
-	}) => {
-		const el = target as HTMLInputElement;
+	const onSubmitEvent = (ev: SyntheticEvent<HTMLFormElement, Event>) => {
+		ev.preventDefault();
+		setValue("");
+		if (props.onSubmit instanceof Function) props.onSubmit(ev);
+	};
+
+	const onChangeEvent = (
+		ev: SyntheticEvent<HTMLInputElement | HTMLTextAreaElement, Event>,
+	) => {
+		const el = ev.target as HTMLInputElement;
+		let newValue = el.value;
+		setValue(newValue);
+
 		if (multiline) {
 			if (autoHeight) {
 				if (el.style.height !== `${minHeight}px`) {
@@ -53,88 +67,90 @@ const Input: FC<IInputProps> = ({
 				props.onMaxLengthExceed();
 
 			if (props.reference?.current) {
-				(props.reference.current as HTMLInputElement).value = (
-					el.value || ""
-				).substring(0, props.maxlength);
+				newValue = newValue.substring(0, props.maxlength);
+				(props.reference.current as HTMLInputElement).value = newValue;
+				setValue(newValue);
 			}
 			return;
 		}
-
-		if (ev && props.onChange instanceof Function) props.onChange({ ev });
 	};
 
 	const clear = () => {
-		const current = {
-			target: props.reference?.current,
-		};
-
 		if (props.reference?.current) {
 			(props.reference.current as HTMLInputElement).value = "";
 		}
 
-		onChangeEvent(current);
+		setValue("");
 	};
 
 	return (
-		<div
+		<form
+			onSubmit={onSubmitEvent}
 			className={classNames("rce-container-input", props.className)}
 			style={style}
 		>
 			{props.leftButtons && (
 				<div className="rce-input-buttons">{props.leftButtons}</div>
 			)}
-			{!multiline ? (
-				<input
-					ref={props.reference as LegacyRef<HTMLInputElement>}
-					type={type}
-					className={classNames("rce-input")}
-					placeholder={placeholder}
-					defaultValue={props.defaultValue}
-					style={props.inputStyle}
-					onChange={(e) =>
-						onChangeEvent({ ev: e, target: props.reference?.current })
-					}
-					onCopy={props.onCopy}
-					onCut={props.onCut}
-					onPaste={props.onPaste}
-					onBlur={props.onBlur}
-					onFocus={props.onFocus}
-					onSelect={props.onSelect}
-					onSubmit={props.onSubmit}
-					onReset={props.onReset}
-					onKeyDown={props.onKeyDown}
-					onKeyUp={props.onKeyUp}
-					value={props.value}
-				/>
-			) : (
-				<textarea
-					ref={props.reference as LegacyRef<HTMLTextAreaElement>}
-					className={classNames("rce-input", "rce-input-textarea")}
-					placeholder={placeholder}
-					defaultValue={props.defaultValue}
-					style={props.inputStyle}
-					onChange={(e) =>
-						onChangeEvent({ ev: e, target: props.reference?.current })
-					}
-					onCopy={props.onCopy}
-					onCut={props.onCut}
-					onPaste={props.onPaste}
-					onBlur={props.onBlur}
-					onFocus={props.onFocus}
-					onSelect={props.onSelect}
-					onSubmit={props.onSubmit}
-					onReset={props.onReset}
-					onKeyDown={props.onKeyDown}
-					onKeyPress={props.onKeyPress}
-					onKeyUp={props.onKeyUp}
-				>
-					{props.defaultValue ? props?.value ?? null : null}
-				</textarea>
-			)}
+			<div className="rce-input-content w-full relative">
+				{!multiline ? (
+					<input
+						name={"chat-input"}
+						ref={props.reference as LegacyRef<HTMLInputElement>}
+						type={type}
+						className={classNames("rce-input w-full")}
+						placeholder={placeholder}
+						value={value}
+						style={props.inputStyle}
+						onChange={(e) => onChangeEvent(e)}
+						onCopy={props.onCopy}
+						onCut={props.onCut}
+						onPaste={props.onPaste}
+						onBlur={props.onBlur}
+						onFocus={props.onFocus}
+						onSelect={props.onSelect}
+						onReset={props.onReset}
+						onKeyDown={props.onKeyDown}
+						onKeyUp={props.onKeyUp}
+					/>
+				) : (
+					<textarea
+						ref={props.reference as LegacyRef<HTMLTextAreaElement>}
+						className={classNames("rce-input", "rce-input-textarea")}
+						placeholder={placeholder}
+						style={props.inputStyle}
+						onChange={(e) => onChangeEvent(e)}
+						onCopy={props.onCopy}
+						onCut={props.onCut}
+						onPaste={props.onPaste}
+						onBlur={props.onBlur}
+						onFocus={props.onFocus}
+						onSelect={props.onSelect}
+						onReset={props.onReset}
+						onKeyDown={props.onKeyDown}
+						onKeyUp={props.onKeyUp}
+					>
+						{value}
+					</textarea>
+				)}
+				{clearButton && value && (
+					<Button
+						className="rce-input-clear"
+						onClick={clear}
+						backgroundColor={"transparent"}
+						style={{ position: "absolute", zIndex: 1, right: 0, opacity: 0.3 }}
+						icon={{
+							component: <MdClear color={"var(--rce-color-gray)"} size={20} />,
+						}}
+					>
+						{clearButton}
+					</Button>
+				)}
+			</div>
 			{props.rightButtons && (
 				<div className="rce-input-buttons">{props.rightButtons}</div>
 			)}
-		</div>
+		</form>
 	);
 };
 
