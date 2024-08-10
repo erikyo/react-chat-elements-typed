213a697fdf21eced1d995257ba8df69e13b36967
Erik Golinelli <erik@codekraft.it>
2024-08-10 10:31:14 +0200

    ChatItem storybook

--- src/ChatItem/ChatItem.stories.tsx
+++ src/ChatItem/ChatItem.stories.tsx
@@ -1,17 +1,15 @@
-import type { Meta, StoryObj } from "@storybook/react";
+import type { StoryObj } from "@storybook/react";
 import ChatItem from "./ChatItem";
-import { chatListArray } from "../stories/example/utils/MessageTypes";
+import { chatListArray } from "../stories/utils/MessageTypes";
 
 const meta = {
 	component: ChatItem,
-} satisfies Meta<typeof ChatItem>;
+};
 
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
-	args: {
-		...chatListArray(),
-	},
+export const Default = {
+	args: chatListArray(),
 };
