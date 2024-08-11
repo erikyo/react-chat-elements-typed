60b3184c40087365b0b2d70c6f227f81cae17097
Erik Golinelli <erik@codekraft.it>
2024-08-11 09:56:31 +0200

    The list of messages component

--- src/MessageList/MessageList.tsx
+++ src/MessageList/MessageList.tsx
@@ -8,27 +8,22 @@ import {
 	useState,
 } from "react";
 import "./MessageList.css";
-
 import MessageBox from "../MessageBox/MessageBox";
-
 import classNames from "classnames";
-import { FaChevronDown } from "react-icons/fa";
-import type {
-	IMessageListProps,
-	MeetingMessageEvent,
-	MessageListEvent,
-	MessageType,
-} from "../types";
+import { MdKeyboardArrowDown } from "react-icons/md";
+import type { IMessageListProps } from "../types";
+import Button from "../Button/Button";
+import Badge from "../Badge/Badge";
 
 const MessageList: FC<IMessageListProps> = ({
 	reference,
 	lockable = false,
 	toBottomHeight = 300,
-	downButton,
+	downButton = null,
 	...props
 }) => {
-	const [scrollBottom, setScrollBottom] = useState(0);
-	const [_downButton, setDownButton] = useState(false);
+	const [scrollBottom, setScrollBottom] = useState<number>(0);
+	const [scrollButton, setScrollButton] = useState<boolean>(false);
 	const prevProps = useRef(props);
 
 	const checkScroll = () => {
@@ -51,7 +46,7 @@ const MessageList: FC<IMessageListProps> = ({
 	useEffect(() => {
 		if (!reference?.current) return;
 
-		if (prevProps.current?.dataSource?.length !== props.dataSource.length) {
+		if (prevProps.current?.dataSource.length !== props.dataSource.length) {
 			setScrollBottom(getBottom(reference.current));
 			checkScroll();
 		}
@@ -66,68 +61,6 @@ const MessageList: FC<IMessageListProps> = ({
 		return 0;
 	};
 
-	const onClick: MessageListEvent = (x, i, e) => {
-		if (props.onClick instanceof Function) props.onClick(x, i, e);
-	};
-
-	const onOpen: MeetingMessageEvent = (item, index, event) => {
-		if (props.onOpen instanceof Function) props.onOpen(item, index, event);
-	};
-
-	const onDownload: MessageListEvent = (item, index, event) => {
-		if (props.onDownload instanceof Function)
-			props.onDownload(item, index, event);
-	};
-
-	const onPhotoError: MessageListEvent = (item, index, event) => {
-		if (props.onPhotoError instanceof Function)
-			props.onPhotoError(item, index, event);
-	};
-
-	const onTitleClick: MessageListEvent = (item, index, event) => {
-		if (props.onTitleClick instanceof Function)
-			props.onTitleClick(item, index, event);
-	};
-
-	const onForwardClick: MessageListEvent = (item, index, event) => {
-		if (props.onForwardClick instanceof Function)
-			props.onForwardClick(item, index, event);
-	};
-
-	const onReplyClick: MessageListEvent = (item, index, event) => {
-		if (props.onReplyClick instanceof Function)
-			props.onReplyClick(item, index, event);
-	};
-
-	const onReplyMessageClick: MessageListEvent = (item, index, event) => {
-		if (props.onReplyMessageClick instanceof Function)
-			props.onReplyMessageClick(item, index, event);
-	};
-
-	const onRemoveMessageClick: MessageListEvent = (item, index, event) => {
-		if (props.onRemoveMessageClick instanceof Function)
-			props.onRemoveMessageClick(item, index, event);
-	};
-
-	const onContextMenu: MessageListEvent = (item, index, event) => {
-		if (props.onContextMenu instanceof Function)
-			props.onContextMenu(item, index, event);
-	};
-
-	const onMessageFocused: (
-		item: MessageType,
-		index: number,
-		isFocused: boolean,
-	) => void = (item, index, isFocused) => {
-		if (props.onMessageFocused instanceof Function)
-			props.onMessageFocused(item, index, isFocused);
-	};
-
-	const onMeetingMessageClick: MessageListEvent = (item, index, event) => {
-		if (props.onMeetingMessageClick instanceof Function)
-			props.onMeetingMessageClick(item, index, event);
-	};
-
 	const onScroll = (e: UIEvent): void => {
 		const element = e?.currentTarget as HTMLDivElement;
 		const bottom = getBottom(element);
@@ -136,13 +69,13 @@ const MessageList: FC<IMessageListProps> = ({
 			toBottomHeight === "100%" ||
 			(toBottomHeight && bottom > Number(toBottomHeight))
 		) {
-			if (!_downButton) {
-				setDownButton(true);
+			if (!scrollButton) {
+				setScrollButton(true);
 				setScrollBottom(bottom);
 			}
 		} else {
-			if (_downButton) {
-				setDownButton(false);
+			if (scrollButton) {
+				setScrollButton(false);
 				setScrollBottom(bottom);
 			}
 		}
@@ -159,16 +92,6 @@ const MessageList: FC<IMessageListProps> = ({
 		if (props.onDownButtonClick instanceof Function) props.onDownButtonClick(e);
 	};
 
-	const onMeetingMoreSelect: MessageListEvent = (item, i, e) => {
-		if (props.onMeetingMoreSelect instanceof Function)
-			props.onMeetingMoreSelect(item, i, e);
-	};
-
-	const onMeetingLinkClick: MessageListEvent = (item, i, e) => {
-		if (props.onMeetingLinkClick instanceof Function)
-			props.onMeetingLinkClick(item, i, e);
-	};
-
 	return (
 		<div
 			className={classNames(["rce-container-mlist", props.className])}
@@ -180,51 +103,27 @@ const MessageList: FC<IMessageListProps> = ({
 				onScroll={(e) => onScroll(e)}
 				className="rce-mlist"
 			>
-				{props.dataSource
-					? props.dataSource.map((x, i: number) => (
-							<MessageBox
-								{...x}
-								key={i.toString()}
-								onOpen={(e: MouseEvent) => onOpen(x, i, e)}
-								onPhotoError={(e: MouseEvent) => onPhotoError(x, i, e)}
-								onDownload={(e) => onDownload(x, i, e)}
-								onTitleClick={
-									props.onTitleClick && ((e) => onTitleClick(x, i, e))
-								}
-								onForwardClick={(e) => onForwardClick(x, i, e)}
-								onReplyClick={(e) => onReplyClick(x, i, e)}
-								onReplyMessageClick={(e) => onReplyMessageClick(x, i, e)}
-								onRemoveMessageClick={(e) => onRemoveMessageClick(x, i, e)}
-								onClick={(e) => onClick(x, i, e)}
-								onContextMenu={(e) => onContextMenu(x, i, e)}
-								onMeetingMoreSelect={(e: MouseEvent) =>
-									onMeetingMoreSelect(x, i, e)
-								}
-								onMessageFocused={(r) => onMessageFocused(x, i, r)}
-								onMeetingMessageClick={(e) => onMeetingMessageClick(x, i, e)}
-								onMeetingTitleClick={props.onMeetingTitleClick}
-								onMeetingVideoLinkClick={props.onMeetingVideoLinkClick}
-								onMeetingLinkClick={(e) => onMeetingLinkClick(x, i, e)}
-								actionButtons={props.actionButtons}
-								style={props.messageBoxStyles}
-								notchStyle={props.notchStyle}
-							/>
-						))
-					: null}
+				{props.dataSource?.map((x, i: number) => (
+					<MessageBox {...x} {...props} key={`message-${i.toString()}`} />
+				))}
 			</div>
-			{downButton === true && _downButton && toBottomHeight !== "100%" && (
-				<div
+			{scrollButton && toBottomHeight !== "100%" && (
+				<Button
+					rounded
 					className="rce-mlist-down-button"
 					onClick={(e) => toBottom(e)}
-					onKeyDown={console.log}
+					color={"var(--rce-color-gray)"}
+					backgroundColor={"var(--rce-color-white)"}
+					icon={{ component: <MdKeyboardArrowDown size={40} /> }}
+					style={{ padding: 2 }}
 				>
-					<FaChevronDown />
-					{props.downButtonBadge !== undefined ? (
-						<span className="rce-mlist-down-button--badge">
-							{props.downButtonBadge.toString()}
-						</span>
-					) : null}
-				</div>
+					{props?.downButtonBadge ? (
+						<Badge
+							className="rce-mlist-down-button--badge"
+							value={props?.downButtonBadge}
+						/>
+					) : undefined}
+				</Button>
 			)}
 		</div>
 	);
