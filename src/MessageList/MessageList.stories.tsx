909160e50c685eaef3d733eb19285294b9fd0f33
Erik Golinelli <erik@codekraft.it>
2024-08-11 09:56:59 +0200

    The list of messages storybook

--- src/MessageList/MessageList.stories.tsx
+++ src/MessageList/MessageList.stories.tsx
@@ -1,18 +1,15 @@
 import type { StoryObj } from "@storybook/react";
 
 import MessageList from "./MessageList";
-import {
-	systemMessage,
-	textMessage,
-} from "../stories/example/utils/MessageTypes.ts";
 
 import { randomMessage } from "../stories/example/components/RandomMessage";
+import { systemMessage, textMessage } from "../stories/utils/MessageTypes";
 
 const meta = {
 	component: MessageList,
 	parameters: {
 		backgrounds: {
-			default: "dark",
+			default: "green",
 			values: [
 				{ name: "green", value: "#35A782" },
 				{ name: "dark", value: "#aaa", default: true },
@@ -27,14 +24,10 @@ type Story = StoryObj<typeof meta>;
 
 export const Default = {
 	args: {
-		downButton: true,
 		dataSource: [
-			systemMessage,
+			{ ...systemMessage(), text: "system message", id: "system" },
 			...Array.from({ length: 10 }).map((_, i) => {
 				return textMessage(i);
-			}),
-			...Array.from({ length: 5 }).map((_, i) => {
-				return randomMessage("random", i);
 			}),
 		],
 	},
