cb4c76e720d66b1323fcebd193e8d1f7e28fe27b
Erik Golinelli <erik@codekraft.it>
2024-08-13 20:02:12 +0200

    splitting avatar into multiple components

--- src/Avatar/Avatar.tsx
+++ src/Avatar/Avatar.tsx
@@ -46,6 +46,37 @@ function getAvatarLetters(
 		.join("");
 }
 
+const LetterAvatar: FC<IAvatarProps> = (props) => {
+	return (
+		<div
+			className={"rce-avatar rce-avatar-letter-background"}
+			style={{
+				backgroundColor: stringToColor(
+					props.letterItem?.id ?? props?.title ?? "unknown",
+				),
+			}}
+		>
+			<svg
+				role={"img"}
+				xmlns="http://www.w3.org/2000/svg"
+				viewBox="0 0 50 50"
+				className="rce-avatar-letter"
+			>
+				<text
+					fill={"white"}
+					fontSize="25"
+					x="50%"
+					y="50%"
+					dominantBaseline="central"
+					textAnchor="middle"
+				>
+					{getAvatarLetters(props?.letterItem, props?.title)}
+				</text>
+			</svg>
+		</div>
+	);
+};
+
 const Avatar: FC<IAvatarProps> = ({
 	size = "default",
 	rounded = true,
@@ -62,38 +93,12 @@ const Avatar: FC<IAvatarProps> = ({
 			style={props?.style}
 		>
 			{props.letterItem || !props?.src ? (
-				<div
-					className={"rce-avatar rce-avatar-letter-background"}
-					style={{
-						backgroundColor: stringToColor(
-							props.letterItem?.id ?? props?.title ?? "unknown",
-						),
-					}}
-				>
-					<svg
-						role={"img"}
-						xmlns="http://www.w3.org/2000/svg"
-						viewBox="0 0 50 50"
-						className="rce-avatar-letter"
-					>
-						<text
-							fill={"white"}
-							fontSize="25"
-							x="50%"
-							y="50%"
-							dominantBaseline="central"
-							textAnchor="middle"
-						>
-							{getAvatarLetters(props?.letterItem, props?.title)}
-						</text>
-					</svg>
-				</div>
+				<LetterAvatar {...props} />
 			) : (
 				<img
 					loading={"lazy"}
 					alt={props?.alt}
-					src={props?.src}
-					onError={props?.onError}
+					src={props.src}
 					className={"rce-avatar"}
 				/>
 			)}
